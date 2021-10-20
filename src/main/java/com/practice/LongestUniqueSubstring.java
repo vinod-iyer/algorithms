c8ca260182c2d0c1f235be0a84808ab6e3e62bdb
Vinod Iyer <vinod.iyer@tomtom.com>
2021-10-20 16:51:49 +0530

    initial commit jgd
    Signed-off-by: Vinod Iyer <vinod.iyer@tomtom.com>

--- src/main/java/com/practice/LongestUniqueSubstring.java
+++ src/main/java/com/practice/LongestUniqueSubstring.java
@@ -1,13 +1,13 @@
 package com.practice;
 
 public class LongestUniqueSubstring {
+
     public int getLengthOfLongestUniqueSubstring(String input) {
         boolean[] occurenceArray = new boolean[128];
         int lengthOfSubstring = 0;
         int maxLengthSoFar = 0;
         for (int index = 0; index < input.length(); ++index) {
             int asciiOfChar = (int) input.charAt(index);
-
             if (occurenceArray[asciiOfChar]) {
                 System.out.println("Size of substrings " + lengthOfSubstring);
                 if (maxLengthSoFar < lengthOfSubstring) {
